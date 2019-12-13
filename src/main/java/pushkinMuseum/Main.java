acd88ff44b55ff3531b104f0912f5a6b0962545b
Yulia Melkina <52936292+koluchikrot@users.noreply.github.com>
2019-12-13 11:58:46 +0300

    New exhibition

--- src/main/java/pushkinMuseum/Main.java
+++ src/main/java/pushkinMuseum/Main.java
@@ -5,13 +5,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-//import pushkinMuseum.model.ExhibitListWrapper;
+import pushkinMuseum.model.AncientPiece;
 import pushkinMuseum.model.Exhibition;
-//import pushkinMuseum.model.ExhibitionDataWrapper;
 import pushkinMuseum.model.Painting;
 import pushkinMuseum.view.ExhibitionLayoutController;
 import pushkinMuseum.view.HallLayoutController;
@@ -20,7 +18,6 @@ import pushkinMuseum.view.ItemLayoutController;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.prefs.Preferences;
 
 public class Main<T> extends Application {
@@ -46,10 +43,19 @@ public class Main<T> extends Application {
         hall1.add(new Painting("Рождение Венеры", "hall1", "An incredible painting", true, "-",
                 "Боттичелли", "1486", 2785, 1725, materials1));
         hall1.add(new Painting("Бичевание Христа", "hall1", "An incredible painting 2", true, "file:resources/test1.JpG",
-                "Иоганн Кербеке", "1457", 930, 650, new ArrayList<String>()));
+                "Иоганн Кербеке", "1457", 930, 650, new ArrayList<>()));
 
         exhibitionData.add(new Exhibition("Картины", hall1, "Выставка картин эпохи Возрождения"));
 
+        ObservableList<AncientPiece> hall2 = FXCollections.observableArrayList();
+        hall2.add(new AncientPiece("Гидрия чернофигурная с изображением петухов", "Античное искусство. Кипр. Древняя Греция. Древний Рим", "В число крупнейших центров восточно-греческой вазописи, существовавших в VI в. до н.э. на побережье Малой Азии, входят Клазомены, располагавшиеся в Северной Ионии. Здесь около середины VI столетия, под влиянием керамики Коринфа и Аттики, складывается своя школа чернофигурных росписей. Их отличает темно-коричневый, почти черный лак силуэтных изображений, обильное применение тонкой и аккуратной гравировки, а также накладных красок, белой и пурпурной, положенных непосредственно на глину. Гидрии – одна из наиболее распространенных форм сосудов у клазоменских мастеров. Четкие, немного жестковатые контуры ваз, рельефные валики и выступы указывают на возможные металлические прототипы.\n" +
+                "\n" +
+                "Московская гидрия целиком покрыта черным лаком, за исключением «корзины» лучей над ножкой и двух клейм с росписью – на плечиках и тулове. В первом, обрамленном сверху узкой полоской язычков, изображены четыре сирены, во втором - два петуха в геральдической композиции. Лица и грудь сирен окрашены белой краской, перья крыльев всех существ переданы тщательной гравировкой, детали оперения сирен и петухов, гребни, бородки и шеи последних дополнены пурпуром и полосками мелких белых точек.\n" +
+                "\n" +
+                "Изображения петухов и сирен нередки в клазоменской керамике, прежде всего в группе, получившей название группа Урла (по названию современного местечка близ Клазомен). Росписи входящих в нее амфор и гидрий отличает любовь к полихромии, хотя рисунок не всегда четкий и аккуратный. Большинство ваз и фрагментов датируется между 540 – 520 гг. до н.э. Примечательно, что гидрии этой группы представлены почти исключительно фрагментами, московский сосуд – единственный целый экземпляр. Его великолепная сохранность объясняется тем, что ваза была найдена в погребении, случайно открытом близ станицы Таманская в 1929 году. Первоначально сосуд хранился в музее г. Темрюка. Во время Великой Отечественной войны он пропал, как и многие памятники крымских и южнорусских музеев. В 1960-е годы гидрия «всплыла» в Германии и была приобретена у частного лица Государственным собранием скульптуры Дрездена. Благодаря ранней, еще довоенной публикации вазы в одном из немецких археологических журналов, было установлено ее происхождение, и в 1969 году гидрия поступила в ГМИИ им. А.С.Пушкина.",
+                true, "-", "третья четверть VI века до н.э.", "Клазомен"));
+
+        exhibitionData.add(new Exhibition("Античное искусство", hall2, "Экспонаты античности"));
         //saveExhibitionDataToFile((ObservableList<T>) hall1, new File("database/data.xml"));
     }
 
